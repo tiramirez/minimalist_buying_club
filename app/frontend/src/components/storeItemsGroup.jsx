de96624d40f097c7a8fb96634673b3ca141208c1
Tomas Ramirez <tiramirez@uc.cl>
2024-06-07 18:56:20 -0400

    Handle error 500 to fetch items with hardcoded example

--- app/frontend/src/components/storeItemsGroup.jsx
+++ app/frontend/src/components/storeItemsGroup.jsx
@@ -2,80 +2,58 @@ import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import { NumericFormat } from 'react-number-format';
 import './storeItemsGroup.css';
+import Items from "../static/sample_products.json";
 
-const items = [
-    {
-        'id': 1,
-        'productName': 'Tomatoes',
-        'productUnitPrice':2.00,
-        'selectedAmount':0
-    },
-    {
-        'id': 2,
-        'productName': 'Lemon',
-        'productUnitPrice':4.00,
-        'selectedAmount':0
-    },
-    {
-        'id': 3,
-        'productName': 'Lettuce',
-        'productUnitPrice':1.00,
-        'selectedAmount':0
-    },
-    {
-        'id': 4,
-        'productName': 'Onions',
-        'productUnitPrice':4.00,
-        'selectedAmount':0
-    },
-]
+// https://react.dev/learn/state-a-components-memory
+const items_fallback = Items.Items
 
 // const handleClickAdd = (comment_id, user_id) => {
 //     setButtonLikes((prevValue) => prevValue + 1)
-//     LikeComment(comment_id, user_id)
+//     LikeComment(comment_id, user_id  )
 // }
 // const handleClickRemove = (comment_id, user_id) => {
 //     setButtonLikes((prevValue) => prevValue - 1)
 //     LikeComment(comment_id, user_id)
 // }
+const ItemBox = ({ Item }) => {
+    return (
+        <div className="itemBlock">
+            <div className="selectedAmount">
+                {/* onClick={handleClickRemove(comment_id, user_id)} */}
+                <button className="circleBtn BtnLeft">-</button>
+                {/* {Item.selectedAmount} */}
+                0
+                {/* onClick={handleClickAdd(comment_id, user_id)} */}
+                <button className="circleBtn BtnRight">+</button>
+            </div>
+            <div className="productName">{Item.product_name}</div>
+            <div className="productUnitPrice"><NumericFormat value={Item.unit_price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} /></div>
+            <div className="productTotal">$0.00</div>
+        </div>
+    );
+};
+
 
 const ItemsGroup = () => {
-    // const [users, setUsers] = useState([]);
-    // useEffect(() => {
-    //     axios.get('/users/').then((response) => {
-    //         setUsers(response.data);
-    //     }).catch((error) => {
-    //         console.error('Error fetching data:', error);
-    //     });
-    // }, []);
-
-    // return (
-    //     <div> <h2>User List</h2>
-    //         <ul>
-    //             {users.map((user) => (<li key={user.id}>{user.email}</li>))}
-    //         </ul>
-    //     </div>
-    // );
+    const [items, setUsers] = useState([]);
+    useEffect(() => {
+        axios.get('/users/')
+        .then((response) => {setUsers(response.data);})
+        .catch((error) => {
+            if (error.response.status===500){
+                setUsers(items_fallback);
+            } else {
+                console.log(error.stack);
+                console.error('Error fetching data:', error);}
+            });
+    }, []);
 
     return (
         <div className="itemsGroup">
             <h2>Items List</h2>
-        <div>
-        {items.map((singleItem) => (
-            <div className="itemBlock">
-                <div className="selectedAmount">
-                {/* onClick={handleClickRemove(comment_id, user_id)} */}
-                    <button className="circleBtn BtnLeft" >-</button>
-                    {singleItem.selectedAmount}
-                    {/* onClick={handleClickAdd(comment_id, user_id)} */}
-                    <button className="circleBtn BtnRight">+</button>
-                </div>
-                <div className="productName">{singleItem.productName}</div>
-                <div className="productUnitPrice"><NumericFormat value={singleItem.productUnitPrice.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'}/></div>
-                <div className="productTotal">$0.00</div>
+            <div>
+                {items.map((singleItem) => (<ItemBox Item={singleItem}/>))}
             </div>
-        ))}
-        </div>
         </div>
     );
 };
