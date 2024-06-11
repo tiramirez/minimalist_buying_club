c3777e9436d21e0c1ce18b706f9e8e8779d4cdca
Tomas Ramirez <tiramirez@uc.cl>
2024-06-11 15:01:13 -0400

    Enable select qty with buttons

--- app/frontend/src/components/storeItemsGroup.jsx
+++ app/frontend/src/components/storeItemsGroup.jsx
@@ -1,34 +1,43 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { Component, useEffect, useState } from 'react';
 import { NumericFormat } from 'react-number-format';
 import './storeItemsGroup.css';
 
-// const handleClickAdd = (comment_id, user_id) => {
-//     setButtonLikes((prevValue) => prevValue + 1)
-//     LikeComment(comment_id, user_id  )
-// }
-// const handleClickRemove = (comment_id, user_id) => {
-//     setButtonLikes((prevValue) => prevValue - 1)
-//     LikeComment(comment_id, user_id)
-// }
-function ItemBox({ Item }) {
-    return (
-        <div className="itemBlock" key={Item.id}>
-            <div className="selectedAmount">
-                {/* onClick={handleClickRemove(comment_id, user_id)} */}
-                <button className="circleBtn BtnLeft">-</button>
-                {/* {Item.selectedAmount} */}
-                0
-                {/* onClick={handleClickAdd(comment_id, user_id)} */}
-                <button className="circleBtn BtnRight">+</button>
-            </div>
-            <div className="productName">{Item.product_name}</div>
-            <div className="productUnitPrice"><NumericFormat value={Item.unit_price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} /></div>
-            <div className="productTotal">$0.00</div>
-        </div>
-    );
-};
+class ItemBox extends Component {
+    constructor(props) {
+        super(props);
+        this.state = { quantity: 0 };
+      }
+  
+    handleIncrement = () => {
+      this.setState({
+        quantity: this.state.quantity + 1 
+      });
+    };
 
+    handleReduction = () => {
+        if (this.state.quantity > 0) {
+            this.setState({
+                quantity: this.state.quantity - 1 
+            });
+        }
+      };
+  
+    render() {
+        return (
+            <div className="itemBlock" key={this.props.Item.id}>
+                <div className="selectedAmount">
+                    <button className="circleBtn BtnLeft" onClick={this.handleReduction}>-</button>
+                    <p className="productQuantity">{this.state.quantity}</p>
+                    <button className="circleBtn BtnRight" onClick={this.handleIncrement}>+</button>
+                </div>
+                <div className="productName">{this.props.Item.product_name}</div>
+                <div className="productUnitPrice"><NumericFormat value={this.props.Item.unit_price.toFixed(2)} displayType={'text'} thousandSeparator={true} prefix={'$'} /></div>
+                <div className="productTotal">$0.00</div>
+            </div>
+        );
+    }
+  }
 
 function ItemsGroup() {
     const [items, setUsers] = useState([]);
