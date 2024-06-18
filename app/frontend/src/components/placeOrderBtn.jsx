f625b33dbca515738773e1307ebe8bf9693dc9f8
Tomas Ramirez <tiramirez@uc.cl>
2024-06-17 17:56:12 -0700

    onClick={submitOrder} collects email and selected products

--- app/frontend/src/components/placeOrderBtn.jsx
+++ app/frontend/src/components/placeOrderBtn.jsx
@@ -1,34 +1,50 @@
 import axios from 'axios';
-import React from "react";
+import React, { useState } from "react";
 import './placeOrderBtn.css';
 
 
 
 // function retieveItems() {
 //     // Query all items which value is greater than 0
-//     const emailInput = document.getElementById("email");
-//     const passwordInput = document.getElementById("password");
+//     const emailtextarea = document.getElementById("email");
+//     const passwordtextarea = document.getElementById("password");
 
-//     const email = emailInput.value;
-//     const password = passwordInput.value;
+//     const email = emailtextarea.value;
+//     const password = passwordtextarea.value;
 // };
 
-function submitOrder() {
-  axios.post("/placeOrder/", {
-      email: 'test@email.com',
-      products:{'prod1':0,'prod2':3}
-    })
-    .then((response) => {
-      console.log(response);
-    });
-};
 
 
-export default function PlaceOrderBtn(){
-    return(
-        <div>
-            <button onClick={submitOrder}>Place Order</button>
-        </div>
-    )
+export default function PlaceOrderBtn({ productsList }) {
+  const [answerEmail, updateEmail] = useState("");
+
+  function handleChange(event) {
+    // console.log(event.target.value);
+    updateEmail(event.target.value);
+  }
+
+  function submitOrder() {
+    const api = process.env.REACT_APP_API_ITEMS
+    const endpoint = 'test_fast_api/';
+    const api_url = api + endpoint
+
+    const content = {
+      'email': answerEmail,
+      'products': JSON.stringify(productsList.filter(item => item.product_quantity !== 0))
+    }
+    console.log(content)
+    // axios.post(api_url, content )
+    //   .then((response) => {
+    //     console.log(response);
+    //   });
+  };
+
+
+  return (
+    <div>
+      <textarea key="textareaEmail" onChange={handleChange}> Your Email.</textarea>
+      <button onClick={submitOrder}>Place Order</button>
+    </div>
+  )
 
 };
