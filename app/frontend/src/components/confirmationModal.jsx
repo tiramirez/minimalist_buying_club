8819291ce39b91b70cc4d77f114f2c1a65faef02
Tomas Ramirez <tiramirez@uc.cl>
2024-08-04 22:22:34 -0400

    Format checkout confirmation modal

--- app/frontend/src/components/confirmationModal.jsx
+++ app/frontend/src/components/confirmationModal.jsx
@@ -15,11 +15,11 @@ function AlertModal({ show, onCloseButtonClick, message}) {
             ) : (
               <div>
                 <h2 className="text-2xl font-bold mb-4">{message.title}</h2>
-                <div className="mb-6">
-                    <h4>{message.body}</h4>
+                <div className="text-xl mb-6">
+                    <p>{message.body}</p>
                 </div>
-                <div className="flex justify-end space-x-4">
-                  <button className="bg-gray-500 text-white py-1 px-4 rounded hover:bg-gray-700" onClick={onCloseButtonClick}>Close</button>
+                <div className="flex space-x-4">
+                  <button className="bg-blue-500 text-white py-1 px-4 rounded hover:bg-gray-700" onClick={onCloseButtonClick}>Close</button>
                 </div>
               </div>
             )}
