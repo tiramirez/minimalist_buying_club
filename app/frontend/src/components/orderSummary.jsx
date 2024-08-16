80aa1f05c2397a7362c05f4b364f33fa891fb251
Tomas Ramirez <tiramirez@uc.cl>
2024-08-15 21:59:18 -0400

    Move My Cart to an independant button

--- app/frontend/src/components/orderSummary.jsx
+++ app/frontend/src/components/orderSummary.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { NumericFormat } from 'react-number-format';
 
-export default function Summary({ productsList, handleDeleteCart, clickOnCheckout }) {
+export default function Summary({ productsList, showMyCart, handleMyCart, handleDeleteCart, clickOnCheckout }) {
     const [orderSubtotal, updateSubtotal] = useState(0.00);
     const [products] = useState(productsList);
 
@@ -17,11 +17,15 @@ export default function Summary({ productsList, handleDeleteCart, clickOnCheckou
         handleDeleteCart();
       }
 
+    function clickOnMyCart() {
+        handleMyCart();
+      }
+
     const serviceFee = 4.00;
     const orderTotal = orderSubtotal + serviceFee;
 
     return (
-        <div className="p-4 md:shadow md:rounded-lg md:bg-white bg-gray-200 w-full md:w-80">
+        <div className="p-4 md:shadow md:rounded-lg md:bg-white bg-gray-200 w-full md:w-96">
           <h1 className="inline md:hidden text-3xl font-sans" >PanPan Store</h1>
           <div className="overflow-x-auto">
             <table className="w-full text-left text-sm text-gray-800">
@@ -47,7 +51,8 @@ export default function Summary({ productsList, handleDeleteCart, clickOnCheckou
               </tbody>
             </table>
           </div>
-            <div className="mt-1 flex w-full justify-between">
+            <div className="mt-1 flex w-full justify-between space-x-1">
+                <button className={(!showMyCart?"bg-zinc-400 text-gray-400":"text-white bg-violet-500 ")+"rounded  font-semibold px-2 md:py-1 text-md md:text-sm hover:bg-blue-600 hover:text-white rounded"} onClick={clickOnMyCart}>In my Cart</button>
                 <button className="px-2 md:py-1 text-md md:text-sm bg-red-500 text-white rounded hover:bg-red-700" onClick={clickOnDeleteCart}>Reset Cart</button>
                 <button className="px-2 md:py-1 text-md md:text-sm bg-green-500 text-white rounded hover:bg-green-700" onClick={clickOnCheckout}>Checkout</button>
           </div>
--- app/frontend/src/components/storeAisles.jsx
+++ app/frontend/src/components/storeAisles.jsx
@@ -3,23 +3,25 @@ import './storeAisles.css';
 // import '../output.css';
 
 // https://react.dev/learn/rendering-lists 
-function AislesNav({ Categories, handleFilter }) {
+function AislesNav({ Categories, showMyCart, handleFilter }) {
     return (
       <div key="navMenuContainer" className="bg-inherit md:bg-gray-100 md:rounded-lg">
         <div className="inline md:hidden bg-inherit text-xl">
-          <select 
-            name="filterDropdown" id="filterDropdown"
-            className='w-full bg-inherit text-white'
-            onChange={(e) => {
-                console.log(e);
-                handleFilter(e.target.value);
-              }}>
-            {Categories.map((aisle) => (
-              <option key={aisle.id} id={aisle.id} className='bg-gray-100' value={aisle.name}>
-                  {aisle.name}
-                </option>
-            ))}
-          </select>
+          {showMyCart?<>Order details</>:
+            <select 
+              name="filterDropdown" id="filterDropdown"
+              className='w-full bg-inherit text-white'
+              onChange={(e) => {
+                  console.log(e);
+                  handleFilter(e.target.value);
+                }}>
+              {Categories.map((aisle) => (
+                <option key={aisle.id} id={aisle.id} className='bg-gray-100' value={aisle.name}>
+                    {aisle.name}
+                  </option>
+              ))}
+            </select>
+          }
         </div>
         <div className="hidden md:inline">
         <h2 className="text-lg font-semibold text-gray-700 mb-4 p-4 pb-0">Aisles</h2>
