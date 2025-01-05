d32b3f7ba99127a8f86dbbbfbc7ecb75dca165e0
Tomas Ramirez <tiramirez@uc.cl>
2025-01-05 10:20:13 -0300

    [fix] clean code

--- app/frontend/src/components/checkoutModal.jsx
+++ app/frontend/src/components/checkoutModal.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState, useMemo } from "react";
+import React, { useEffect, useState } from "react";
 import axios from 'axios';
 // import './newsletterModal.css';
 import { LayoutComponent } from './layout/modal'
