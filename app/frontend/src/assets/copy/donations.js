0c4e3d2a1a193fe145e7806e4d5faefd60c300c1
Tomas Ramirez <tiramirez@uc.cl>
2026-01-05 15:55:17 -0300

    [update] checkout donation box

--- app/frontend/src/assets/copy/donations.js
+++ app/frontend/src/assets/copy/donations.js
@@ -1,4 +1,15 @@
 export const mainDonation = {
+  title: "Philly People of Hope Donation",
+  description: "Philly People of Hope is a local, community-led effort to provide fresh groceries, hot meals, and other resources to neighbors experiencing homelessness or food insecurity. The produce, eggs, bread, and other products PanPan donates every week are picked up on Saturday afternoons and given away either as raw ingredients or as nutritious meals prepared by a team of volunteer cooks. Your donations allow PanPan to purchase high quality food from our local suppliers to be donated to community members in need.",
+  donationOptions: [
+    { label: "No donation", value: 0 },
+    { label: "$1", value: 1 },
+    { label: "$2", value: 2 },
+    { label: "$5", value: 5 },
+  ]
+}
+
+export const oldDonation = {
   title: "Sharing Excess Donation",
   description: "Sharing Excess is a nationwide food rescue nonprofit started in Philly by a Drexel student. The produce, eggs, bread, milk, and meat products PanPan donates get picked up and redistributed to free community giveaways on the same day. Your donations will allow PanPan to purchase high quality food from our local suppliers to be given away to the community!",
   donationOptions: [
